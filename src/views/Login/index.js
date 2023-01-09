178b896b9c9ffaacd1a5a53009315d2e49d695c2
Sergio FCU <sergiofcu@ext.inditex.com>
2023-01-09 14:16:28 +0100

    cambio de tamaño en el login

--- src/views/Login/index.js
+++ src/views/Login/index.js
@@ -1,13 +1,14 @@
-import { Box, Heading, Page, PageContent, Paragraph } from 'grommet';
+import { Box, Heading, Paragraph } from 'grommet';
 
 import Form from '../../common/components/Form';
+import Layout from '../../common/components/Layout';
 import useLogin from './useLogin';
 
 const Login = () => {
   const { schemaLogin, onLoginWithEmail } = useLogin();
   return (
-    <Page kind="narrow" justify="center" pad="small">
-      <PageContent gap="small" align="center">
+    <Layout align="center">
+      <Box width="medium" gap="small">
         <Box pad="small">
           <Heading fill margin="none" textAlign="center">
             Iniciar Sesion
@@ -21,8 +22,8 @@ const Login = () => {
           schema={schemaLogin}
           onClickSubmit={(value) => onLoginWithEmail(value)}
         />
-      </PageContent>
-    </Page>
+      </Box>
+    </Layout>
   );
 };
 
