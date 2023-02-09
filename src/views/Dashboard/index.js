087de6bf748812c2a004d4d94f36c3e80f07a1da
SergioFc1989 <sergiofc@hotmail.es>
2023-02-09 01:43:14 +0100

    fix: imagen dashboard

--- src/views/Dashboard/index.js
+++ src/views/Dashboard/index.js
@@ -1,10 +1,10 @@
-import { Image } from 'grommet';
-import Main from '../../common/components/Main';
+import { Image, ResponsiveContext } from 'grommet';
+import { useContext } from 'react';
 import MyApp from '../../common/assets/app.svg';
+import Main from '../../common/components/Main';
 
-const Dashboard = () => (
-  <Main>
-    <Image fit="contain" src={MyApp} />
-  </Main>
-);
+const Dashboard = () => {
+  const size = useContext(ResponsiveContext);
+  return <Main>{size === 'large' && <Image fit="contain" src={MyApp} />}</Main>;
+};
 export default Dashboard;
